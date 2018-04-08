29df12db696838e1542f1075c1e6412eda8866e9
Chronaxia <972625961@qq.com>
2018-04-08 10:32:32 +0800

    test

--- app/src/main/java/com/miaxis/escort/LoginActivity.java
+++ app/src/main/java/com/miaxis/escort/LoginActivity.java
@@ -2,6 +2,7 @@ package com.miaxis.escort;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toast.makeText(this, "sadasgsdfdfhdh", Toast.LENGTH_LONG).show();
+
     }
 }
